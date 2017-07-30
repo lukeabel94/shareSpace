c914a24759a5b6e80f7c99284e2c4fdbc4cc4f09
Luke Abel <label017@hotmail.com>
2017-07-30 14:24:54 +1000

    added in search bar

--- js/map.js
+++ js/map.js
@@ -6,6 +6,16 @@ function startMap() {
          zoom: 18
        });
 
+       // Create the search box and link it to the UI element.
+        var input = document.getElementById('pac-input');
+        var searchBox = new google.maps.places.SearchBox(input);
+        map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
+
+        // Bias the SearchBox results towards current map's viewport.
+        map.addListener('bounds_changed', function() {
+          searchBox.setBounds(map.getBounds());
+        });
+
        //load geoJson info
        //map.data.loadGeoJson(*example json*);
 
